1468316498494ed6b36062eecc04b21ebf6cff15
zcky <255608195+zplusplus0life@users.noreply.github.com>
2026-02-08 15:42:29 +0700

    test(check-role): setup function

--- tests/Unit/middleare/CheckRoleTest.php
+++ tests/Unit/middleare/CheckRoleTest.php
@@ -2,14 +2,31 @@
 
 namespace Tests\Unit\middleare;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Http\Middleware\CheckRole;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
 
 class CheckRoleTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+    
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config(['database.default' => 'sqlite']);
+        config(['database.connections.sqlite.database' => ':memory:']);
+
+        $this->artisan('migrate');
+        
+    }
+
+    /** @test */
+    public function guest_is_redirected_to_login(): void
     {
         $this->assertTrue(true);
     }
